40f528bd3bbfe4978f0b483b1ca914e2f09ba49d
byeong <qudcks4885@gmail.com>
2024-04-03 20:50:56 +0900

    fix: baseURL 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://dev.stylekey.shop',
+  baseURL: ' https://www.stylekey.shop',
   withCredentials: true,
 });
 
